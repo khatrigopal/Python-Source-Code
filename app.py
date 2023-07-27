2cdfade85fb390f58ac09748253bfbc53bcf3ebe
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-07-27 21:59:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1> This is Testing Environment with some functionality </h1>'
+    return '<h1> This is Testing Environment with some functionality................... </h1>'
