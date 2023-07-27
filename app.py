69b40bf65113c9764a8c0daaf717ede6bd03ec5d
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-07-27 22:14:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1> -------Cloud & Devops is Good practice.................... </h1>'
+    return '<h1> -------Welcome BAck //// Cloud & Devops is Good practice.................... </h1>'
