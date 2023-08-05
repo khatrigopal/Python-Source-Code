490f727a653e0b4e764a8416788b3cd459ef117e
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-08-05 22:13:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1> ----------Welcome to my Zone.............. </h1>'
+    return '<h1> ----------Testing Grafana.............. </h1>'
