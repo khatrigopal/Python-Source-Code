61347490a61e965c5d067cdfe417351770957fde
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-08-05 21:29:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1> -------This is for MFH Project.................... </h1>'
+    return '<h1> -------This is doing for testing.................... </h1>'
